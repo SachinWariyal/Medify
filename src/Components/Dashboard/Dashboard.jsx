a0df8969afb1d86c618f12e0424407d7dae602fd
Sachin <sachinwariyal4@gmail.com>
2024-04-02 13:15:50 +0530

    Project Started

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='dashboard-container'>
+        <p>The health and well being of our patients and their health care team will always be our priority, so we follow the best practices for cleanliness.</p>
+    </div>
+  )
+}
+
+export default Dashboard
